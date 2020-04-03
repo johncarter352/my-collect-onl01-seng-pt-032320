8a2d5a7bdd6f9fd74cbd25f89033768e34824a14
John Carter <johncarter352@gmail.com>
2020-04-03 07:30:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,11 @@
 def my_collect(array)
   i = 0 
-  collection = []
+  valuable = []
   while i < array.length 
-  collection << yield(array[i])
+  valuable << yield(array[i])
   i += 1
   end
+  valuable
 end
   
 
